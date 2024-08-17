827e4af2b44cfed198510ea2f339aa2c4d1cf241
Eduardo Babeto <eduardo.babeto@gmail.com>
2024-08-17 11:28:48 -0300

    feat: listarClientes() implemented

--- src/Banco.java
+++ src/Banco.java
@@ -1,9 +1,15 @@
 import java.util.List;
+import java.util.ArrayList;
 
 public class Banco {
 
 	private String nome;
 	private List<Conta> contas;
+
+	public Banco(String nome) {
+		this.nome = nome;
+		this.contas = new ArrayList<Conta>();
+	}
 
 	public String getNome() {
 		return nome;
@@ -19,6 +25,13 @@ public class Banco {
 
 	public void setContas(List<Conta> contas) {
 		this.contas = contas;
+	}
+
+	public void listarClientes() {
+		System.out.println("=== Lista de Clientes por Conta ===");
+		for (Conta conta: this.contas) {
+			System.out.println(conta.cliente.getNome());
+		}
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,29 @@
 public class Main {
 
 	public static void main(String[] args) {
+		Banco bb = new Banco("Banco do Brasil");
 		Cliente venilton = new Cliente();
+		Cliente eduardo = new Cliente();
 		venilton.setNome("Venilton");
+		eduardo.setNome("Eduardo");
 		
-		Conta cc = new ContaCorrente(venilton);
+		Conta cc1 = new ContaCorrente(venilton);
+		Conta cc2 = new ContaCorrente(eduardo);
 		Conta poupanca = new ContaPoupanca(venilton);
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
+		cc1.depositar(100);
+		cc1.transferir(100, poupanca);
 		
-		cc.imprimirExtrato();
+		cc2.depositar(1000000);
+
+		cc1.imprimirExtrato();
+		cc2.imprimirExtrato();
 		poupanca.imprimirExtrato();
+		bb.getContas().add(cc1);
+		bb.getContas().add(poupanca);
+		bb.getContas().add(cc2);
+
+		bb.listarClientes();
 	}
 
 }
